17e2c0a2c7e0ff81a06e54416c8f00aa8dc0661b
Tadeo Wotoszyn <tadeo.woto@gmail.com>
2026-05-21 10:51:16 -0300

    feat(dependencies/astro.config): server output and astro node dependency added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
