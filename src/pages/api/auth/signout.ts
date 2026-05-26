ac7ac3af32bb304d0da6ce7ac36168f484569684
Tadeo Wotoszyn <tadeo.woto@gmail.com>
2026-05-26 10:05:17 -0300

    fix(signout): arregle el redirect para que redirija al home

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -4,5 +4,5 @@ import { createClient } from "../../../lib/supabase";
 export const GET: APIRoute = async ({ request, cookies, redirect }) => {
   const supabase = createClient({ request, cookies });
   await supabase.auth.signOut();
-  return redirect("/auth/signin");
+  return redirect("/");
 };
