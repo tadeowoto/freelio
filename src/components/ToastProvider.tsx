4ec0d16c0dee4d9a0f8ef6f8adc6b847fb7672c4
Tadeo Wotoszyn <tadeo.woto@gmail.com>
2026-06-30 10:47:02 -0300

    feat(fix): fix de estilos de sonner

--- src/components/ToastProvider.tsx
+++ src/components/ToastProvider.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "sonner";
+import "sonner/dist/styles.css";
 
 export default function ToastProvider() {
   return <Toaster position="bottom-right" theme="light" richColors />;
